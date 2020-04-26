39187861a299d8ee6b4b864e08243b90ebb24ba7
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-04-25 21:41:45 -0300

    implement data transfer between activities

--- app/src/main/java/com/example/tipsy_app/ResultActivity.java
+++ app/src/main/java/com/example/tipsy_app/ResultActivity.java
@@ -3,10 +3,15 @@ package com.example.tipsy_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ResultActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class ResultActivity extends AppCompatActivity implements Button.OnClickListener {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     Button recalculateBtn;
     TextView resultLabel;
@@ -16,13 +21,35 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-
-
+        this.catchReferencesFromLayout();
+        this.registerForUIEvents();
+        this.updateUI();
     }
 
     private void catchReferencesFromLayout(){
         this.recalculateBtn = findViewById(R.id.recalculateBtn);
         this.resultLabel = findViewById(R.id.resultLabel);
         this.infoLabel = findViewById(R.id.infoLabel);
+    }
+
+    private void registerForUIEvents(){
+        this.recalculateBtn.setOnClickListener(this);
+    }
+
+    private void updateUI(){
+        Bundle intentExtras = getIntent().getExtras();
+        int numberOfPersons = intentExtras.getInt(MainActivity.stepperValueId);
+        int tipValue = intentExtras.getInt(MainActivity.tipValueId);
+        double result = intentExtras.getDouble(MainActivity.resultId);
+
+        this.resultLabel.setText(df.format(result));
+        this.infoLabel.setText("Split between " + numberOfPersons + " people, with " + tipValue + "% tip.");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(R.id.recalculateBtn == v.getId()){
+            finish();
+        }
     }
 }
